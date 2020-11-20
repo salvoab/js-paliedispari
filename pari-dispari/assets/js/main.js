64226dc0d53da59be21340e36064e19df108a3b3
salvoab <salvatore.abate85@gmail.com>
2020-11-20 16:13:04 +0100

    Controllo sugli input dell'utente

--- pari-dispari/assets/js/main.js
+++ pari-dispari/assets/js/main.js
@@ -31,5 +31,41 @@ function isEven(number){
 }
 
 //test
-console.log(isEven(4)); //true
-console.log(isEven(9)); //false
+// console.log(isEven(4)); //true
+// console.log(isEven(9)); //false
+
+//Controllo sull'input della stringa pari o dispari
+var keepAsking = true;
+while(keepAsking){
+    var pariDispari = prompt("Scegli pari o dispari");
+    if(pariDispari != null)
+    {
+        pariDispari = pariDispari.toLocaleLowerCase();
+    }
+        
+    if(pariDispari != "pari" && pariDispari != "dispari"){
+        keepAsking = true;
+        alert("ATTENZIONE! Puoi inserire solo pari o dispari");
+    } else{
+        keepAsking = false;
+    }
+}
+console.log("Hai scelto: " + pariDispari);
+
+//Controllo sull'input del numero fra 1 e 5
+var keepAsking = true;
+while(keepAsking){
+    var numeroUtente = prompt("Scegli un numero intero compreso fra 1 e 5");
+    if(numeroUtente != null)
+    {
+        numeroUtente = parseInt(numeroUtente);
+    }
+        
+    if(isNaN(numeroUtente) || numeroUtente <1 || numeroUtente > 5 ){
+        keepAsking = true;
+        alert("ATTENZIONE! Puoi inserire solo un numero intero compreso fra 1 e 5");
+    } else{
+        keepAsking = false;
+    }
+}
+console.log("Hai scelto il numero: " + numeroUtente);
