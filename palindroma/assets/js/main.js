7c9c4d50edfe89576ba5961b72b8d370e883e565
salvoab <salvatore.abate85@gmail.com>
2020-11-20 14:59:18 +0100

    Creata e testata la funzione isPalindrome

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -3,3 +3,27 @@
 * Creare una funzione per capire se la parola inserita è palindroma
 ********************************************************************/
 
+
+function isPalindrome(word){
+    var leftIndex = 0, rightIndex = word.length - 1;
+    var keepChecking = true;
+    while(leftIndex <= rightIndex && keepChecking){
+        if(word[leftIndex] === word[rightIndex]){
+            keepChecking = true;
+            leftIndex++;
+            rightIndex--;
+        } else {
+            keepChecking = false;
+        }
+    }
+
+    if(keepChecking && (leftIndex > rightIndex) ){
+        return true;
+    }
+    return false;
+}
+
+//Test
+console.log( isPalindrome("osso") );
+console.log( isPalindrome("oro") );
+console.log( isPalindrome("orso") );
