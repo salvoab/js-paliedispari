3637632ee8be34a58c0b4babc14f2fdf5bd96e47
salvoab <salvatore.abate85@gmail.com>
2020-11-20 15:22:22 +0100

    word.toLowerCase dentro la funzione isPalindrome
    L'utente inserisce una parola con prompt, i caratteri possono essere sia maiuscoli che minuscoli o mischiati è indifferente per la funzione isPalindrome

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -7,6 +7,7 @@
 function isPalindrome(word){
     var leftIndex = 0, rightIndex = word.length - 1;
     var keepChecking = true;
+    word = word.toLowerCase();
     while(leftIndex <= rightIndex && keepChecking){
         if(word[leftIndex] === word[rightIndex]){
             keepChecking = true;
@@ -24,10 +25,21 @@ function isPalindrome(word){
 }
 
 //Test
-console.log( isPalindrome("osso") );
-console.log( isPalindrome("oro") );
-console.log( isPalindrome("orso") );
-console.log( isPalindrome("corso") );
-console.log( isPalindrome("kayak") );
+// console.log( isPalindrome("osso") );  //true
+// console.log( isPalindrome("oro") );   //true
+// console.log( isPalindrome("orso") );  //false
+// console.log( isPalindrome("corso") ); //false
+// console.log( isPalindrome("kayak") ); //true
+// console.log( isPalindrome("e") );     //true Ho scelto che una singola lettera sia da considerare palindroma
+// console.log( isPalindrome("KaYak") ); //true Ho scelto che per la funzione non faccia differenza fra caratteri minuscoli e maiuscoli
 
 
+do{
+    var parola = prompt("Inserisci una parola e ti dirò se è palindroma");
+} while(parola == null);
+
+if(isPalindrome(parola)){
+    console.log("La parola " + parola + " è palindroma");
+} else{
+    console.log("La parola " + parola + " NON è palindroma");
+}
