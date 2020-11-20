ce0b927c535c257ee1498841b746f09caf7799a1
salvoab <salvatore.abate85@gmail.com>
2020-11-20 14:42:22 +0100

    File e cartelle di base

---
+++ palindroma/assets/js/main.js
@@ -0,0 +1,5 @@
+/*******************************************************************
+* Chiedere all’utente di inserire una parola
+* Creare una funzione per capire se la parola inserita è palindroma
+********************************************************************/
+
